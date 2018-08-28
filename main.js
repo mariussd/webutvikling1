5ceb9c8f7d83dc6fdbf1650545da1a4433d267f9
mariussd <marius.s.dreyer@gmail.com>
2018-08-28 10:35:32 +0200

    add canvas

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-documentationButton = document.getElementById("showDoc");
-mainHeader = document.getElementById("mainHeader");
+let documentationButton = document.getElementById("showDoc");
+let mainHeader = document.getElementById("mainHeader");
+let canvas = document.getElementById("mainCanvas");
+let ctx = canvas.getContext("2d");
 
 documentationButton.addEventListener("click", showDocumentation);
 mainHeader.addEventListener("mouseover", changeColor);
 mainHeader.addEventListener("mouseleave", changeColorBack);
+
+function drawArt() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, 150, 75);
+}
 
 function showDocumentation() {
     console.log("Jadda");
@@ -16,3 +23,5 @@ function changeColor() {
 function changeColorBack() {
     mainHeader.style.color = "black";
 }
+
+drawArt();
